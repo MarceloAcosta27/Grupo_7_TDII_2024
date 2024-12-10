9ebc2cf8cc3725aead8d11566f66291b7b591fa5
Marcelo <marcelo_27790@hotmail.com>
2024-12-10 01:26:29 -0300

    Subiendo AFP3 y AFP4

---
+++ AFP4/App_4_1_Grupo_7_2024/Drivers/API/Src/API_DelayNoBloqueante.c
@@ -0,0 +1,53 @@
+/*
+ * API_DelayNoBloqueante.c
+ *
+ *  Created on: Nov 10, 2024
+ *      Author: Grupo 7
+ */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "API_DelayNoBloqueante.h"
+
+/**
+  * @brief Initialization DelayUnblocking Function
+  * @param delay_t pointer
+  *        tick_t duration
+  * @retval None
+  */
+void delayInit(delay_t *delay, tick_t duration)
+{
+	delay->startTime = HAL_GetTick();
+	delay->duration = duration;
+	delay->running=false;
+}
+/**
+  * @brief Read DelayUnblocking Function
+  * @param delay_t pointer
+  * @retval bool_t
+  */
+bool_t delayRead(delay_t* delay) {
+    if (!delay->running) {  // Si el retardo no estÃ¡ en ejecuciÃ³n
+        delay->startTime = HAL_GetTick();  // Toma la marca de tiempo actual
+        delay->running = true;  // Inicia el retardo
+        return false;           // AÃºn no se ha cumplido el tiempo
+    }
+    else {  // Si el retardo ya estÃ¡ en ejecuciÃ³n
+        tick_t currentTime = HAL_GetTick();
+        if ((currentTime - delay->startTime) >= delay->duration) {
+            delay->running = false;  // Finaliza el retardo
+            return true;             // Tiempo cumplido
+        }
+        return false;  // Tiempo aÃºn no cumplido
+    }
+}
+/**
+  * @brief Write DelayUnblocking Function
+  * @param delay_t pointer
+  *        tick_t duration
+  * @retval bool_t
+  */
+void delayWrite(delay_t *delay, tick_t duration)
+{
+	delay->duration = duration;
+}
